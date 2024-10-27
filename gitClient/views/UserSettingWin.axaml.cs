88209b7fa1ee7b9aa4d94ffbd35ffaddb1719d1a
smalltownmod <cthulhukultist@gmail.com>
2024-10-27 18:10:29 +0100

    UserSettings changed

--- gitClient/views/UserSettingWin.axaml.cs
+++ gitClient/views/UserSettingWin.axaml.cs
@@ -13,11 +13,13 @@ namespace gitClient.views {
 			Close();
 		}
 		private void BtnSubmit_Click(object sender, Avalonia.Interactivity.RoutedEventArgs ev) {
-			GitCred gitCred = new GitCred();
-			gitCred.GitName = GhUsrNamBox.Text.Trim();
-			gitCred.GitPassword = string.Empty;
-			var jFile = JsonSerializer.Serialize(gitCred);
-			File.WriteAllText(("cred.json"), jFile);
+			ProcInvoker.Run("git", $" config --global user.name \"{UsrNamBox.Text}\"");
+			ProcInvoker.Run("git", $" config --global user.email {UsrMailBox.Text}");
+			//	GitCred gitCred = new GitCred();
+			//	gitCred.GitName = GhUsrNamBox.Text.Trim();
+			//	gitCred.GitPassword = string.Empty;
+			//	var jFile = JsonSerializer.Serialize(gitCred);
+			//	File.WriteAllText(("cred.json"), jFile);
 			Close();
 		}
 		public void FirstOpen(Repository r) {
@@ -27,11 +29,11 @@ namespace gitClient.views {
 				UsrMailBox.Text = Autor.Email;	
 			}
 
-			if(File.Exists(@"cred.json")) {
-				var cred = JsonSerializer.Deserialize<GitCred>(File.ReadAllText(@"cred.json")) ?? new GitCred();
-				GhUsrNamBox.Text = cred.GitName;
-				//GhPWBox.Text = cred.GitPassword;
-			}
+			//if(File.Exists(@"cred.json")) {
+			//	var cred = JsonSerializer.Deserialize<GitCred>(File.ReadAllText(@"cred.json")) ?? new GitCred();
+			//	GhUsrNamBox.Text = cred.GitName;
+			//	//GhPWBox.Text = cred.GitPassword;
+			//}
 		}
 	}
 }
